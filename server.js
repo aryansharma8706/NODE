4791e3791f0942ad8df4074c05389e29cf182e1a
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-11 10:04:51 +0530

    `Added initial server.js file with HTTP server creation and basic routing`

---
+++ server.js
@@ -0,0 +1,14 @@
+const server = http.createServer((req,res)=>{
+    if(req.url === '/users'){
+        res.statusCode = 201
+        res.and('users data')
+    }else{
+        res.end('home')
+    }
+
+})
+
+server.listen(5000,()=>{
+    console.log('server is runing');
+    console.log(server);
+})
