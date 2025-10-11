b46b7cef0ccb46791874792fa6d81fbd2fea4ff6
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-11 10:52:15 +0530

    `Refactored Express server and Node.js modules to use ES6 syntax and updated HTTP server to handle requests and responses correctly.`

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
-const express = require('express') ;
-const app = express() ;
+//creating an express server instead of http
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello from the express');
+});
+
+app.listen(3000, () => {
+  console.log('server is running');
+});
