12ce758b88056078df3b002b18a8770839277c00
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-16 13:59:50 +0530

    `Removed userroutes.js and added userRoutes import to App.js`

--- userroutes.js
+++
@@ -1,7 +0,0 @@
-
-const router = XPathExpression.Router();
-const userController = require('../controllers/userController');
-
-router.post('/users', userController.createUser);
-
-module.exports = router;
